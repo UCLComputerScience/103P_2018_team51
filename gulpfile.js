e579a5a00abd77a7d928cd8aabee67cc0fe2b3a0
Leo McArdle <leo.mcardle.17@ucl.ac.uk>
2018-02-10 19:58:29 +0000

    add boostrap, base template

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp')
 var { spawn } = require('child_process')
+var sass = require('gulp-sass')
 
 gulp.task('django:runserver', () => {
   spawn('python3', ['manage.py', 'runserver', '0.0.0.0:8000'], {
@@ -7,6 +8,18 @@ gulp.task('django:runserver', () => {
   })
 })
 
+gulp.task('sass:build', () => {
+  gulp.src('./media/css/base.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./static/css'))
+})
+
+gulp.task('sass:watch', () => {
+  gulp.watch('./media/css/*', ['sass:build'])
+})
+
 gulp.task('default', () => {
+  gulp.start('sass:build')
   gulp.start('django:runserver')
+  gulp.start('sass:watch')
 })
