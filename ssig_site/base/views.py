cbe5daf4f47ee7080ca153b082ccd28a25342738
cameron <cameron@studdstreet.com>
2018-03-01 12:04:08 +0000

    #1 create group model and views - updated

--- ssig_site/base/views.py
+++ ssig_site/base/views.py
@@ -16,10 +16,9 @@ def group(request, id):
 
 def events(request):
     events = models.Event.objects.all()
-    return render(request, 'events.html', { 'events': events })
+    return render(request, 'events.html', {'events': events})
 
 
 def event(request, id):
     event = models.Event.objects.get(id=id)
-    return render(request, 'event.html', { 'event': event })
-
+    return render(request, 'event.html', {'event': event})
