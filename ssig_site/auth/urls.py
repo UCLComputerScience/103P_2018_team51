b988529d778cfd4aae05fe60f010c41fba8d020b
Leo McArdle <leomcardle@gmail.com>
2018-03-05 17:26:19 +0000

    add ssig header with login/logout links

--- ssig_site/auth/urls.py
+++ ssig_site/auth/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.start, name='auth'),
+    path('', views.start, name='login'),
     path('callback', views.callback),
-    path('logout', views.logout),
+    path('logout', views.logout, name='logout'),
 ]
