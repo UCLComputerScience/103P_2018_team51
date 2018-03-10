b0712f8ef571ba5cfba880bae627bfcc8364f85b
cameronmcloughlin <cameron@studdstreet.com>
2018-03-10 17:59:54 +0000

    added join/leave button to group template #4 (#24)

--- ssig_site/base/urls.py
+++ ssig_site/base/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('g/<int:id>', views.group, name='group-detail'),
     path('events', views.events, name='events'),
-    path('e/<int:id>', views.event, name='event')
+    path('e/<int:id>', views.event, name='event'),
+    path('g/<int:id>/join', views.group_join, name='group-join'),
+    path('g/<int:id>/leave', views.group_leave, name='group-leave')
 ]
--- ssig_site/base/views.py
+++ ssig_site/base/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from ssig_site.base.models import Group
 
 from . import models
@@ -11,7 +12,8 @@ def index(request):
 
 def group(request, id):
     this_group = Group.objects.all().get(id=id)
-    return render(request, 'group-detail.html', {'group': this_group})
+    group_member = request.user in this_group.user_set.all()
+    return render(request, 'group-detail.html', {'group': this_group, 'group_member': group_member})
 
 
 def events(request):
@@ -22,3 +24,19 @@ def events(request):
 def event(request, id):
     event = models.Event.objects.get(id=id)
     return render(request, 'event.html', {'event': event})
+
+
+def group_join(request, id):
+    group = models.Group.objects.get(id=id)
+    current_user = request.user
+
+    current_user.interest_groups.add(group)
+    return redirect('group-detail', id)
+
+
+def group_leave(request, id):
+    group = models.Group.objects.get(id=id)
+    current_user = request.user
+
+    current_user.interest_groups.remove(group)
+    return redirect('group-detail', id)
