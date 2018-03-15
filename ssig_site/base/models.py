8a5981e0be59300e85dfa7c2aded42555f07aa7c
Leo McArdle <leomcardle@gmail.com>
2018-03-15 12:19:22 +0000

    add location to events #25

--- ssig_site/base/models.py
+++ ssig_site/base/models.py
@@ -17,6 +17,7 @@ class Event(models.Model):
     description = models.TextField()
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()
+    location = models.CharField(max_length=200)
 
     group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
 
