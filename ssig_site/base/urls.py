6e6f19166c1d2e27b0810b4dc047fe79233260ab
jerrykang97 <32646494+Minjae97@users.noreply.github.com>
2018-03-15 16:56:24 +0000

    Student event registration (add registration button) #22 (#30)

--- ssig_site/base/urls.py
+++ ssig_site/base/urls.py
@@ -4,9 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('g/<int:id>', views.group, name='group-detail'),
+
     path('events', views.events, name='events'),
     path('e/<int:id>', views.event, name='event'),
+    path('e/<int:id>/register', views.event_register, name='event-register'),
+    path('e/<int:id>/unregister', views.event_unregister, name='event-unregister'),
+
+    path('g/<int:id>', views.group, name='group-detail'),
     path('g/<int:id>/join', views.group_join, name='group-join'),
     path('g/<int:id>/leave', views.group_leave, name='group-leave')
 ]
