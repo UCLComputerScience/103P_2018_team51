52656cacbbf21b670b0314a3c5b57e663b655d30
Leo McArdle <leo.mcardle.17@ucl.ac.uk>
2018-04-19 07:52:20 +0100

    fix flake8

--- ssig_site/context_processors.py
+++ ssig_site/context_processors.py
@@ -1,6 +1,7 @@
 from django.conf import settings as s
 
+
 def settings(request):
-  return {
-    'GOOGLE_MAPS_KEY': s.GOOGLE_MAPS_KEY,
-  }
+    return {
+        'GOOGLE_MAPS_KEY': s.GOOGLE_MAPS_KEY,
+    }
